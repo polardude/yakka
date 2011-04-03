957037fef21c060fff2e4c50720ba774f99e4b5b
Andy S <polardude2000@hotmail.com>
2011-04-03 19:48:12 -0400

    Basic functionality completed for tasks
    Can do the following:
    
    *Add tasks
    *Delete tasks
    *Edit tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,4 +3,45 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+  
+  def edit
+    @task = Task.find(params[:id])
+  end
+  
+  def new
+    @task = Task.new
+  end
+  
+  def create
+    @task = Task.new(params[:task])
+    
+    respond_to do |format|
+      if @task.save
+        format.html { redirect_to tasks_path }
+      else
+        format.html { render :action => "new"}
+      end
+    end
+  end
+  
+  def update
+    @task = Task.find(params[:id])
+    
+    respond_to do |format|
+      if @task.update_attributes(params[:task])
+        format.html { redirect_to tasks_path }
+      else
+        format.html { render :action => "edit" }
+      end
+    end
+  end
+  
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+  
+    respond_to do |format|
+      format.html { redirect_to tasks_path }
+    end
+  end
 end
