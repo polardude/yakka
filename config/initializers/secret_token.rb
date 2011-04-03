c1181e98fc2f7631e8e7ae5347c8dcfb52ce1726
Andy S <polardude2000@hotmail.com>
2011-04-03 11:48:49 -0400

    Initial Commit -- Readme has been changed to reflect the current app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yakka::Application.config.secret_token = 'a6ae71a31270d8855bc5c228fb010adc22fa0f6f349670de5c699a94b41f3a3aed3ad174422e8e67864a03b38ba9adfe099570a882d68b941b667c6eec6da279'
