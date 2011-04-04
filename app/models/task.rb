db9eadc09f5da0fd9b00898a32154169fbcd5853
Andy S <polardude2000@hotmail.com>
2011-04-03 20:22:52 -0400

    Finished category model and tested for basic functionality in console

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,9 +1,12 @@
 class Task < ActiveRecord::Base
+  belongs_to :category
+  
   validates :content, :presence => true
   after_initialize :default_values
   
   private
     def default_values
       self.done ||= false
+      self.category_id ||= 1
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110403183234) do
+ActiveRecord::Schema.define(:version => 20110403235810) do
+
+  create_table "categories", :force => true do |t|
+    t.string   "category"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "tasks", :force => true do |t|
     t.text     "content"
@@ -19,6 +25,7 @@ ActiveRecord::Schema.define(:version => 20110403183234) do
     t.boolean  "done"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "category_id"
   end
 
 end
